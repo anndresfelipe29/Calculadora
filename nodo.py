0bbbaf382842bdcd5dc3f30a462efcd3b6064a3f
anndresfelipe29 <30865375+anndresfelipe29@users.noreply.github.com>
2017-08-23 02:35:08 -0500

    Subida de programa calcuadora con arbol y cola
    Subida de programa calcuadora con arbol y cola

---
+++ nodo.py
@@ -0,0 +1,19 @@
+class Nodo:
+	def __init__(self, valor, Padre):
+		self.valor=valor
+		self.Izquierdo=None
+		self.Derecho=None
+		self.Padre=Padre
+		print "Nuevo nodo"
+		
+
+	
+
+
+#	def CrearHijoDer(self):
+#		self.Derecho=Nodo(valor,GetValorRaiz())
+
+#	def CrearHijoDer(self):
+#		Izquierdo=Nodo(valor,GetValorRaiz())
+
+
